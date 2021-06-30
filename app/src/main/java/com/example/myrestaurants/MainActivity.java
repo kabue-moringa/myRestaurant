a4f5dddd896c014553325d93d5fa3c2ceb2a24f7
kabue-moringa <joseph.wangari@student.moringaschool.com>
2021-07-01 01:09:22 +0300

    add changes

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -8,24 +8,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)TextView mAppNameTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mFindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+
+        ButterKnife.bind(this);
+
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View v) {
                 String location = mLocationEditText.getText().toString();
-//                Log.d(TAG, location);
                 Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
                 intent.putExtra("location", location);
                 startActivity(intent);
